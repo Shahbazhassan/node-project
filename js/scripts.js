b749fafd1cdd9b7a35a64b4ae02c876edc9a088c
Shahbaz <shahbazhassan24@gmail.com>
2018-12-13 21:47:57 +0500

    After Task Runners

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#loginButton").click(function(e){
+        e.preventDefault();
+        $('#loginModal').modal('show');
+    });
+    $("#reservationButton").click(function(){
+        $('#reservetableModal').modal('show');
+    });
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
